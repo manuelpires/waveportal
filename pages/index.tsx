bd3bad5973e67270b623ae4b7517d533251854c9
manuelpires <manuelpiresok@gmail.com>
2021-10-11 01:56:03 +0200

    Updated Home page with title and bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
 
 const Home = () => (
   <Layout title="Wave Portal | Manuel Pires">
-    <main>
-      <h1>Welcome to Wave Portal!</h1>
+    <main className={styles.page}>
+      <h1 className={styles.title}>👋 Hey there!</h1>
+      <p className={styles.bio}>
+        I&apos;m Manuel, a software engineer from Argentina. Connect your
+        Ethereum wallet on the Rinkeby testnet and wave at me!
+      </p>
     </main>
   </Layout>
 );

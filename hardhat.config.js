b8be125be958de530700e1060c190077ec4ec782
manuelpires <manuelpiresok@gmail.com>
2021-10-27 02:53:47 +0200

    Updated hardhat config for deploying locally only

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,17 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
+const isLocalEnv = process.env.ALCHEMY_KEY && process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.7",
-  networks: {
-    rinkeby: {
-      url: process.env.ALCHEMY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+  // Only add networks for deploying locally
+  ...(isLocalEnv && {
+    networks: {
+      rinkeby: {
+        url: process.env.ALCHEMY_KEY,
+        accounts: [process.env.PRIVATE_KEY],
+      },
     },
-  },
+  }),
 };
